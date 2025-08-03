15f0fc07a0ff500da9ef53212cb258ef1cd2dbdb
AbdoulSalam407 <abdoulsalamd742@gmail.com>
2025-08-03 21:46:00 +0000

    first commit

--- api/admin_urls.py
+++ api/admin_urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
-    path('logout/', LogoutView.as_view(next_page='http://localhost:3000/'), name='logout'),
+    path('logout/', LogoutView.as_view(next_page='https://ecolefoot-mbnk-ecolefoot-frontend.vercel.app/'), name='logout'),
     path('', admin.site.urls),
 ]
