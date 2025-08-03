ecfd26190edb0cc930b8d509af6de67c60da4208
AbdoulSalam407 <abdoulsalamd742@gmail.com>
2025-08-03 19:56:27 +0000

    first commit

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
-# build.sh
-#!/usr/bin/env bash
-python manage.py collectstatic --no-input
-python manage.py migrate
+# # build.sh
+# #!/usr/bin/env bash
+# python manage.py collectstatic --no-input
+# python manage.py migrate
+# #!/usr/bin/env bash
+# poetry install
 #!/usr/bin/env bash
 poetry install
+poetry run python manage.py collectstatic --no-input
+poetry run python manage.py migrate
